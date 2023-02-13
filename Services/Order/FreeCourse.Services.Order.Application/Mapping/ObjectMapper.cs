4f0b833e0b3e822d5c4ae81f1a237e4f92c48d15
Baran <byasarn@gmail.com>
2023-02-13 15:54:10 +0300

    Dto ve CQRS pattern

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using AutoMapper.Internal.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class ObjectMapper
+    {
+        //normalde eger domain apide olsaydı program.cs de tanımlanarak kullanılabilirdi ama farklı bir class libraryde oldugumuz için custom olarak injection yapılması gerekiyordu o yuzden bu methodlara ihtiyaç var!
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+            return config.CreateMapper();
+        });
+
+        //mapper cağıgrana kadar yukarıdaki kodlar çalışmayacak!
+        public static IMapper Mapper => lazy.Value;
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.Order.Domain.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext:DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options):base(options)
+        {
+
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+        public DbSet<Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<Domain.OrderAggregate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+            //address id si olmadıgı ve bir tablosu olmadıgı için owned olarak işaretlenir
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(x => x.Address).WithOwner();
+            
+
+            
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
